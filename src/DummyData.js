39068bdb8bdfb3630a118bc2322d3e863218892e
mitsu0428 <mitsuprograming@gmail.com>
2022-07-15 14:16:28 +0900

    new

---
+++ src/DummyData.js
@@ -0,0 +1,22 @@
+export const PostsData = [
+  {
+    id: 1,
+    name: "testUser1",
+    content: "TestContents01",
+  },
+  {
+    id: 2,
+    name: "testUser2",
+    content: "TestContents02",
+  },
+  {
+    id: 3,
+    name: "testUser3",
+    content: "TestContents03",
+  },
+  {
+    id: 4,
+    name: "testUser4",
+    content: "TestContents04",
+  },
+];
