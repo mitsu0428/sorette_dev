184c0320aa82c8e28351f7111ad6e1ce687e92d4
mitsu0428 <mitsuprograming@gmail.com>
2022-07-17 11:30:28 +0900

    add(thumnail)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import TopPageThumb from './components/TopPageThumb';
 import HomePage  from './components/HomePage';
 import Contact from './components/Contact';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -10,6 +11,7 @@ const App = () => {
     <Router>
       <div className="App">
         <Header />
+        <TopPageThumb />
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="Contact" element={<Contact />} />
---
+++ src/components/TopPageThumb.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopPageThumb = () => {
+  return (
+    <div className='container-thumbnail'>
+        <img src={`${process.env.PUBLIC_URL}/sorette_banner2.png`} alt="top画像" />;
+    </div>
+  )
+}
+
+export default TopPageThumb
